816f3d1cff61f7dd3c3b7fb047fd270f4d052701
4refr0nt <4refr0nt@gmail.com>
2016-10-10 22:03:20 +0500

    clear retain

---
+++ node.js/official-demo/clear-retain.js
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////
+//
+// Clear all messages with RETAIN flag
+//
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+var config = require("./config");
+var host = config.host;
+var port = config.port;
+var user = config.user;
+var pass = config.pass;
+////////////////////////////////////////////////
+
+var mqtt = require('mqtt');
+var opt = {
+  host       : host,
+  port       : port,
+  username   : user,
+  password   : pass,
+  clientId   : 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+  protocolId : 'MQTT',
+  connectTimeout: 3000
+};
+
+var client  = mqtt.connect(opt);
+
+client.on('connect', function () {
+  console.log('Broker connected');
+  client.subscribe('#');
+});
+
+client.on('error', function () {
+  console.log('error');
+});
+
+client.on('offline', function () {
+  console.log('offline');
+});
+
+client.on('message', function (topic, message) {
+  if (message.toString().length > 0) {
+    console.log('receive ' + message.toString())
+    client.publish(topic, null, {retain: true});
+    console.log('clear topic ' + topic)
+  }
+});
+
